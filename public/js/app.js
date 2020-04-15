aabd18bdc2e4efd6cdcc3d0a7496ed4cf199f0f6
Jose <rodrjose213@gmail.com>
2020-04-15 11:39:40 -0400

    setup for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,7 @@
+
+
 const getWeather = (address = '') => {
-    fetch(`http://localhost:3000/weather?address=${address}`).then((response) => {
+    fetch(`/weather?address=${address}`).then((response) => {
         response.json().then((data) => {
             if (data.error) {
                 messageOne.textContent = 'Error';
--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public');
@@ -78,11 +79,6 @@ app.get('/weather', (req, res) => {
         }
     });
 
-    // res.send({
-    //     forecast: 'Snow',
-    //     location: 'Victor New York',
-    //     address: req.query.address
-    // })
 })
 
 app.get('/products', (req, res) => {
@@ -99,6 +95,6 @@ app.get('*', (req, res) => {
     });
 })
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000.');
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}.`);
 });
